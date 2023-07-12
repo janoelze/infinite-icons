246a60fb70624998ebebce5c69396cf69088d712
Jan Oelze <janoelzelebt@googlemail.com>
2023-07-12 03:38:11 +0200

    Update

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Icons
 
-| Icon | Icon | Icon | Icon |
+|  |  |  |  |
 | ---- | ---- | ---- | ---- |
 | ![wind](icons/wind.svg) | ![x-circle](icons/x-circle.svg) | ![x-octagon](icons/x-octagon.svg) | ![x-square](icons/x-square.svg)
+| wind | x-circle | x-octagon | x-square
 | ![x](icons/x.svg) | ![youtube](icons/youtube.svg) | ![zap-off](icons/zap-off.svg) | ![zap](icons/zap.svg)
+| wind | x-circle | x-octagon | x-square | x | youtube | zap-off | zap
 
--- generate-readme.sh
+++ generate-readme.sh
@@ -6,16 +6,19 @@ create_readme(){
 
   echo "# Icons" > README.md
   echo "" >> README.md
-  echo "| Icon | Icon | Icon | Icon |" >> README.md
+  echo "|  |  |  |  |" >> README.md
   echo "| ---- | ---- | ---- | ---- |" >> README.md
   # iterate over icons/*.svg in chunks of four
   row=""
+  titles=""
   for icon in icons/*.svg; do
     iconname=$(basename $icon)
     iconname=${iconname%.*}
     row="$row| ![$iconname]($icon) "
+    titles="$titles| $iconname "
     if [ $(echo $row | wc -w) -eq 8 ]; then
       echo $row >> README.md
+      echo $titles >> README.md
       row=""
     fi
   done
