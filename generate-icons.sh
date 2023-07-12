544e8eefd022129dd36d120484541debb6df1b3e
Jan Oelze <janoelzelebt@googlemail.com>
2023-07-12 03:56:31 +0200

    Update

--- generate-icons.sh
+++ generate-icons.sh
@@ -12,7 +12,7 @@ unlink_icons(){
 generate_icon(){
   icon=$1
   echo "Generating icon '$icon'..."
-  echo "The icon you are creating is called '$icon.svg'. The SVG element should have the class 'icon icon-$icon'." | llm --system "You are an icon creation engine. The theme is dystopian. Only output valid SVG code. NEVER respond with prose. NEVER respond with markdown. NEVER respond with comments, NEVER respond with notes. Only output valid SVG." -m gpt-4 > "icons/$icon.svg"
+  echo "The icon you are creating is called '$icon.svg'. The SVG element should have the class 'icon icon-$icon'." | llm --system "You are an icon creation engine. Only output valid SVG code. NEVER respond with prose. NEVER respond with markdown. NEVER respond with comments, NEVER respond with notes. Only output valid SVG." -m gpt-4 > "icons/$icon.svg"
   clean_yaml "icons/$icon.svg"
 }
 
