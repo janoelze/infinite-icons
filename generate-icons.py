44a1cf0932dfc5cc91c811b995db8cfea169371c
Jan Oelze <janoelzelebt@googlemail.com>
2023-07-12 11:38:09 +0200

    Update

--- generate-icons.py
+++ generate-icons.py
@@ -1,7 +1,7 @@
-import os
-import subprocess
+import os, sys, random
+import subprocess, threading
 
-def clean_yaml(file_path):
+def clean_svg_file(file_path):
     with open(file_path, 'r') as file:
         lines = file.readlines()
     
@@ -9,11 +9,6 @@ def clean_yaml(file_path):
         for line in lines:
             if not line.startswith('```'):
                 file.write(line)
-
-def unlink_icons():
-    if os.path.exists('icons'):
-        shutil.rmtree('icons')
-    os.mkdir('icons')
 
 def generate_icon_ids():
     output = subprocess.check_output(['llm', 'Output 15 icons potentially included in a large web icon set. The names should contain descriptive IDs. Make them non-obvious. Output each line starting with a "*" star. The filname should only contain the desceriptive icon ID. For example: icon-home.svg, icon-pencil.svg and so forth. NEVER respond with prose. NEVER respond with markdown. NEVER respond with comments, NEVER respond with notes.'], text=True)
@@ -26,28 +21,29 @@ def generate_icon(icon):
     output = subprocess.check_output(['llm', '--system', f"You are an icon creation engine. Only output valid SVG code. NEVER respond with prose. NEVER respond with markdown. NEVER respond with comments, NEVER respond with notes. Only output valid SVG.", '-m', 'gpt-4', prompt], text=True)
     with open(f"icons/{icon}.svg", 'w') as file:
         file.write(output)
-    clean_yaml(f"icons/{icon}.svg")
+    clean_svg_file(f"icons/{icon}.svg")
 
-def generate_icons():
-    i = 0
-
+def get_missing_icon():
     with open("icons.txt", 'r') as file:
         lines = file.readlines()
-
-    for line in lines:
+        line = random.choice(lines).strip()
         if line.startswith('*'):
-            raw_icon = line[1:].strip()
-            basename = os.path.basename(raw_icon)
-            icon = os.path.splitext(basename)[0]
-            path = f"icons/{icon}.svg"
-            if not os.path.isfile(path) and i < 4:
-                generate_icon(icon)
-                i += 1
+            line = line[1:].strip()
+        basename = os.path.basename(line)
+        icon = os.path.splitext(basename)[0]
+        if 'icon' not in icon:
+            icon = f"icon-{icon}"
+        return icon
+
+def generate_icons(amount):
+    for icon in range(amount):
+        threading.Thread(target=generate_icon, args=(get_missing_icon(),)).start()
 
 def main():
     try:
+        count = int(sys.argv[1] if len(sys.argv) > 1 else None)
         generate_icon_ids()
-        generate_icons()
+        generate_icons(count)
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
     except FileNotFoundError:
