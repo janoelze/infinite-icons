856d6e61b734c1485be23148b00fbdaebda6990e
Jan Oelze <janoelzelebt@googlemail.com>
2023-07-13 00:31:12 +0200

    Update

--- generate-icons.py
+++ generate-icons.py
@@ -1,5 +1,13 @@
 import os, sys, random
 import subprocess, threading
+import openai
+
+def run_prompt(system, prompt):
+    c = openai.ChatCompletion.create(model="gpt-4", messages=[
+        {"role": "system", "content": system},
+        {"role": "user", "content": prompt}
+    ])
+    return c.choices[0].message.content
 
 def clean_svg_file(file_path):
     with open(file_path, 'r') as file:
@@ -11,7 +19,8 @@ def clean_svg_file(file_path):
                 file.write(line)
 
 def generate_icon_ids():
-    output = subprocess.check_output(['llm', 'Output 15 icons potentially included in a large web icon set. The names should contain descriptive IDs. Make them non-obvious. Output each line starting with a "*" star. The filname should only contain the desceriptive icon ID. For example: icon-home.svg, icon-pencil.svg and so forth. NEVER respond with prose. NEVER respond with markdown. NEVER respond with comments, NEVER respond with notes.'], text=True)
+    output = run_prompt("", "Output 15 icons potentially included in a large web icon set. The names should contain descriptive IDs. Make them non-obvious. Output each line starting with a '*' star. The filname should only contain the desceriptive icon ID. For example: icon-home.svg, icon-pencil.svg and so forth. NEVER respond with prose. NEVER respond with markdown. NEVER respond with comments, NEVER respond with notes.")
+    # output = subprocess.check_output(['llm', 'Output 15 icons potentially included in a large web icon set. The names should contain descriptive IDs. Make them non-obvious. Output each line starting with a "*" star. The filname should only contain the desceriptive icon ID. For example: icon-home.svg, icon-pencil.svg and so forth. NEVER respond with prose. NEVER respond with markdown. NEVER respond with comments, NEVER respond with notes.'], text=True)
     with open('icons.txt', 'a+') as file:
         file.write(output)
 
@@ -19,10 +28,13 @@ def generate_icon(icon_filename):
     icon_id = os.path.splitext(icon_filename)[0]
     print(f"Generating icon '{icon_id}'")
     prompt = f"The icon you are creating is called '{icon_id}'. The SVG element should have the class 'icon {icon_id}'."
-    output = subprocess.check_output(['llm', '--system', f"You are an icon creation engine. Only output valid SVG code. NEVER respond with prose. NEVER respond with markdown. NEVER respond with comments, NEVER respond with notes. Only output valid SVG.", '-m', 'gpt-4', prompt], text=True)
+    output = run_prompt(
+        f"You are an icon creation engine. Only output valid SVG code. NEVER respond with prose. NEVER respond with markdown. NEVER respond with comments, NEVER respond with notes. Only output valid SVG.",
+        prompt
+    )
     with open(f"icons/{icon_id}.svg", 'w') as file:
         file.write(output)
-    clean_svg_file(f"icons/{icon_filename}.svg")
+    clean_svg_file(f"icons/{icon_id}.svg")
 
 def clean_line(line):
     line = line.strip()
@@ -51,7 +63,7 @@ def generate_icons(amount):
 def main():
     try:
         count = int(sys.argv[1] if len(sys.argv) > 1 else None)
-        # generate_icon_ids()
+        generate_icon_ids()
         generate_icons(count)
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
