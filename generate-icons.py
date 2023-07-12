5c13f9bf8c433084df366cac56e84617597d3fdd
Jan Oelze <janoelzelebt@googlemail.com>
2023-07-12 23:49:45 +0200

    Update generate-icons.py

--- generate-icons.py
+++ generate-icons.py
@@ -15,25 +15,34 @@ def generate_icon_ids():
     with open('icons.txt', 'a+') as file:
         file.write(output)
 
-def generate_icon(icon):
-    print(f"Generating icon '{icon}'...")
-    prompt = f"The icon you are creating is called '{icon}.svg'. The SVG element should have the class 'icon icon-{icon}'."
+def generate_icon(icon_filename):
+    icon_id = os.path.splitext(icon_filename)[0]
+    print(f"Generating icon '{icon_id}'")
+    prompt = f"The icon you are creating is called '{icon_id}'. The SVG element should have the class 'icon {icon_id}'."
     output = subprocess.check_output(['llm', '--system', f"You are an icon creation engine. Only output valid SVG code. NEVER respond with prose. NEVER respond with markdown. NEVER respond with comments, NEVER respond with notes. Only output valid SVG.", '-m', 'gpt-4', prompt], text=True)
-    with open(f"icons/{icon}.svg", 'w') as file:
+    with open(f"icons/{icon_id}.svg", 'w') as file:
         file.write(output)
-    clean_svg_file(f"icons/{icon}.svg")
+    clean_svg_file(f"icons/{icon_filename}.svg")
+
+def clean_line(line):
+    line = line.strip()
+    if line.startswith('*'):
+        line = line[1:].strip()
+    if 'icon' not in line:
+        line = f"icon-{line}"
+    return line
 
 def get_missing_icon():
+    icon_filenames = []
     with open("icons.txt", 'r') as file:
         lines = file.readlines()
-        line = random.choice(lines).strip()
-        if line.startswith('*'):
-            line = line[1:].strip()
-        basename = os.path.basename(line)
-        icon = os.path.splitext(basename)[0]
-        if 'icon' not in icon:
-            icon = f"icon-{icon}"
-        return icon
+        for line in lines:
+            icon_filename = clean_line(line)
+            icon_filenames.append(icon_filename)
+    icon_filenames = random.sample(icon_filenames, len(icon_filenames))
+    for icon_filename in icon_filenames:
+        if not os.path.exists(f"icons/{icon_filename}"):
+            return icon_filename
 
 def generate_icons(amount):
     for icon in range(amount):
@@ -42,7 +51,7 @@ def generate_icons(amount):
 def main():
     try:
         count = int(sys.argv[1] if len(sys.argv) > 1 else None)
-        generate_icon_ids()
+        # generate_icon_ids()
         generate_icons(count)
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
